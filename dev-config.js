f6a18e355488025fdd7990de2ead7cd2d85cfc7b
mgmeyers <matthew.gregory.meyers@gmail.com>
2021-03-21 10:00:32 -0700

    Use es6 module format for dev config

--- dev-config.js
+++ dev-config.js
@@ -1,3 +1,1 @@
-module.exports = {
-    pluginDestination: '',
-}
+export const pluginDestination = "";
--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,35 @@
-import typescript from '@rollup/plugin-typescript';
-import {nodeResolve} from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
+import typescript from "@rollup/plugin-typescript";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
 
-export default {
-  input: 'main.ts',
-  output: {
-    dir: '.',
-    sourcemap: 'inline',
-    format: 'cjs',
-    exports: 'default'
+import { pluginDestination } from "./dev-config";
+
+const output = [
+  {
+    input: "main.ts",
+    output: {
+      dir: ".",
+      sourcemap: "inline",
+      format: "cjs",
+      exports: "default",
+    },
+    external: ["obsidian"],
+    plugins: [typescript(), nodeResolve({ browser: true }), commonjs()],
   },
-  external: ['obsidian'],
-  plugins: [
-    typescript(),
-    nodeResolve({browser: true}),
-    commonjs(),
-  ]
-};
+];
+
+if (pluginDestination) {
+  output.push({
+    input: "main.ts",
+    output: {
+      dir: pluginDestination,
+      sourcemap: "inline",
+      format: "cjs",
+      exports: "default",
+    },
+    external: ["obsidian"],
+    plugins: [typescript(), nodeResolve({ browser: true }), commonjs()],
+  });
+}
+
+export default output;
